0f0a3f191fcdf75118333db472cb649b9afb9e98
Yersultan <erickrut559559@gmail.com>
2026-01-26 00:24:38 +0500

    add variables.py to lab-1

---
+++ python lab-1/variables.py
@@ -0,0 +1,23 @@
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
