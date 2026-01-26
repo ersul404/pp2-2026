7088f6b0ba501f6ad89496b32bf6441ae7e81672
Yersultan <erickrut559559@gmail.com>
2026-01-26 11:47:59 +0500

    new example

--- python lab-1/syntax.py
+++ python lab-1/syntax.py
@@ -1,2 +1,18 @@
 if 5 > 2:
     print("Five is greater than two!")
+
+
+#variables in python
+x = 5
+y = "Hello, World!"
+
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+
+print("Python is fun!")
+
+
+#syntax error
+if 5 > 2:
+print("Five is greater than two!")
