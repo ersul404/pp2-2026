33806abf9f117ebc193113e736c77ba12fb5c428
Yersultan <erickrut559559@gmail.com>
2026-01-26 11:52:13 +0500

    new example

--- python lab-1/data_types.py
+++ python lab-1/data_types.py
@@ -4,3 +4,33 @@ print(type(x))
 
 y = ["apple", "banana", "cherry"]
 print(type(y))
+
+
+
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
+x = range(10)
+
+#display x:
+print(x)
+
+#convert to list to display the content of x:
+print(list(x))
+
+
+
+x = frozenset({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
