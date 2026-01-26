eeb847b7124fff2dff1afea49772c2adf3364648
Yersultan <erickrut559559@gmail.com>
2026-01-26 11:53:30 +0500

    new example

--- python lab-1/comments.py
+++ python lab-1/comments.py
@@ -1,4 +1,24 @@
 print("Hello, world!") #this is a comment
 
 #print("Hello, world!")
-print("Cheers, Mate!")``
+print("Cheers, Mate!")
+
+
+#This is a comment
+print("Hello, World!")
+
+
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
