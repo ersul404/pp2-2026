94e10592bcecb7ded6de2a6b2d2de970232a6315
Yersultan <erickrut559559@gmail.com>
2026-01-31 01:51:54 +0500

    add tasks for while_loop

---
+++ python lab-2/loops/while_loop.py
@@ -0,0 +1,41 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+
+
+x = 0
+
+while x < 9:
+  print(x)
+  x = x + 1
+
+
+
+n = int(input())
+i = 1
+
+while i <= n:
+    print(i)
+    i += 1
+
+
+
+total = 0
+x = int(input())
+
+while x != 0:
+    total += x
+    x = int(input())
+
+print(total)
+
+
+
+
+n = int(input())
+
+while n > 0:
+    print(n)
+    n -= 1
