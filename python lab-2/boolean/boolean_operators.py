1ebbdfd7089641acc54c5fc7cc97797a4415b693
Yersultan <erickrut559559@gmail.com>
2026-01-31 01:36:30 +0500

    add tasks for boolean_operators

---
+++ python lab-2/boolean/boolean_operators.py
@@ -0,0 +1,32 @@
+x = 5
+
+print(x > 0 and x < 10)
+
+
+
+x = 5
+
+print(x < 5 or x > 10)
+
+
+
+x = 5
+
+print(not(x > 3 and x < 10))
+
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+
+print(x is not y)
