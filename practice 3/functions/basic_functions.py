3c26803da5964e426bcfa7f0ae7e95d846ac1674
Yersultan <erickrut559559@gmail.com>
2026-02-08 17:48:22 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ practice 3/functions/basic_functions.py
@@ -0,0 +1,30 @@
+def my_function():
+  print("Hello from a function")
+
+
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+
+
+#valid fnctions names:
+calculate_sum()
+_private_function()
+myFunction2()
+
+
+
+def show_sum():
+    print(2 + 3)
