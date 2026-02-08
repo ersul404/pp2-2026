265dc0d0e1f3ed5bca4cf4fe3303c945a5311b96
Yersultan <erickrut559559@gmail.com>
2026-02-08 17:54:26 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ practice 3/functions/return_values.py
@@ -0,0 +1,14 @@
+def get_five():
+    return 5
+
+def sum_two(a, b):
+    return a + b
+
+def square(x):
+    return x * x
+
+def is_even(n):
+    return n % 2 == 0
+
+def max_of_two(a, b):
+    return a if a > b else b
