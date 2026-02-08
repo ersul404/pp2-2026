feaee11c0182a95fc910d95cfac830fef6722520
Yersultan <erickrut559559@gmail.com>
2026-02-08 18:09:20 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance example

---
+++ practice 3/classes/class_definition.py
@@ -0,0 +1,21 @@
+class MyClass:
+  x = 5
+
+
+class Point:
+    x = 0
+    y = 0
+
+
+class User:
+    username = ""
+    email = ""
+
+
+class BankAccount:
+    balance = 0
+
+
+class Rectangle:
+    width = 0
+    height = 0
