521f2e1acca6cf3626d20a3b949a2d3fa4fcab14
Yersultan <erickrut559559@gmail.com>
2026-02-22 16:00:01 +0500

    Add Practice4 - Python dates

---
+++ practice 4/dates/4.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+date1 = datetime(2026, 2, 22, 12, 0, 0)
+date2 = datetime(2026, 2, 20, 10, 30, 0)
+
+difference = date1 - date2
+
+seconds = difference.total_seconds()
+
+print("Difference in seconds:", int(seconds))
