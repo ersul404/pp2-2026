4b3d07e34e346ba89d48b397ee328e5034c3b70f
Yersultan <erickrut559559@gmail.com>
2026-02-22 18:19:24 +0500

    Add Practie4 - Python json

---
+++ practice 4/json/1.py
@@ -0,0 +1,21 @@
+import json
+
+FILE_NAME = "sample-data.json"
+
+with open(FILE_NAME, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+print("Interface Status")
+print("=" * 80)
+print(f'{"DN":50} {"Description":20} {"Speed":8} {"MTU":6}')
+print(f'{"-"*50} {"-"*20}  {"-"*6}  {"-"*6}')
+
+for item in data["imdata"]:
+    attrs = item["l1PhysIf"]["attributes"]
+
+    dn = attrs.get("dn", "")
+    descr = attrs.get("descr", "")
+    speed = attrs.get("speed", "")
+    mtu = attrs.get("mtu", "")
+
+    print(f"{dn:50} {descr:20}  {speed:6}  {mtu:6}")
